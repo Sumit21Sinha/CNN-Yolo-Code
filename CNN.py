0e5eeb61c3572fc6095b2cf7f7c49252643340c0
Sumit <sumit525sinha@gmail.com>
2025-05-11 11:07:42 +0530

    Function output names changed for better understanding

--- CNN.py
+++ CNN.py
@@ -7,8 +7,8 @@ kernel2 = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 def padding(array, p):
     new_image = np.pad(array, pad_width=p, mode='constant', constant_values=0)
     return new_image
-new_image = padding(image,1)
-print(new_image)
+padded_image = padding(image,1)
+print(padded_image)
 def filtering(old_array, new_array, array2):
     kernel_height, kernel_width=array2.shape
     array_height, array_width=old_array.shape
@@ -17,11 +17,11 @@ def filtering(old_array, new_array, array2):
         for y in range(array_width):
             result[x,y]=np.sum(new_array[y:y+kernel_height, x:x+kernel_width]*array2)
     return result
-result = filtering(image, new_image, kernel)
-print(result)
+filtered_image = filtering(image, padded_image, kernel)
+print(filtered_image)
 def relu(array):
     return np.maximum(0, array)
-relu_output=relu(result)
+relu_output=relu(filtered_image)
 print(relu_output)
 def max_pool(array, pool_size=2, stride=1):
     H, W = array.shape
@@ -35,8 +35,8 @@ def max_pool(array, pool_size=2, stride=1):
     return result1
 pooled_matrix=max_pool(relu_output,2,1)
 print(pooled_matrix)
-new_image2=padding(pooled_matrix, 1)
-filtered_matrix=filtering(pooled_matrix, new_image2, kernel2)
-relu_output2=relu(filtered_matrix)
-finalMatrix=max_pool(relu_output2,2,1)
-print(finalMatrix)
+padded_image2=padding(pooled_matrix, 1)
+filtered_image2=filtering(pooled_matrix, padded_image2, kernel2)
+relu_output2=relu(filtered_image2)
+pooled_matrix2=max_pool(relu_output2,2,1)
+print(pooled_matrix2)
